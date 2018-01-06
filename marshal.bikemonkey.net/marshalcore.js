f566785211001d4ad5253e9fc318838ee4176199
Carlos Perez <clayperez@gmail.com>
2018-01-05 19:50:47 -0800

    update

--- marshal.bikemonkey.net/marshalcore.js
+++ marshal.bikemonkey.net/marshalcore.js
@@ -3,6 +3,26 @@
 ////////////////////
 /// Dependencies
 /// --------------------------
-/// VueJS: 
-/// Firebase: 
+/// https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.9/vue.min.js
+/// https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.1/vue-router.min.js
+/// https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.3.4/vue-resource.min.js
+/// https://unpkg.com/vuetify/dist/vuetify.min.js
+/// https://www.gstatic.com/firebasejs/4.2.0/firebase.js
 
+
+//////////////
+// FIREBASE //
+//////////////
+const firebaseConfig = {
+  apiKey: 'AIzaSyACYJ2gQFAaOIQHUQKGuChJy_uJjpUtW8o',
+  authDomain: 'routr-acd70.firebaseapp.com',
+  databaseURL: 'https://routr-acd70.firebaseio.com',
+  projectId: 'routr-acd70',
+  storageBucket: 'routr-acd70.appspot.com',
+  messagingSenderId: '705237275855'
+};
+firebase.initializeApp(firebaseConfig);
+const googleAuth = new firebase.auth.GoogleAuthProvider();
+const database = firebase.database();
+
+console.log("HEY! MARHSALCORE IS UP VIA RAWGIT.");
