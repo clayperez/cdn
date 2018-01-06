f9d2f5c3e3bf50617684cdedcc085f8bee6ebde4
Carlos Perez <clayperez@gmail.com>
2018-01-05 20:14:30 -0800

    building

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cdn
+# CDN STORE FOR RAWGIT
+Use https://rawgit.com/ for delivering the files in this repository.
+
 
--- marshal.bikemonkey.net/marshalcore.js
+++ marshal.bikemonkey.net/marshalcore.js
@@ -1,28 +1,52 @@
 ////////////////////
 // MARSHALCORE.JS //
 ////////////////////
-/// Dependencies
-/// --------------------------
-/// https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.9/vue.min.js
-/// https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.1/vue-router.min.js
-/// https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.3.4/vue-resource.min.js
-/// https://unpkg.com/vuetify/dist/vuetify.min.js
-/// https://www.gstatic.com/firebasejs/4.2.0/firebase.js
+/// Overview
+/// Marshalcore supports the web app built for the Bike Monkey Marshal program
+///
+/// Dependencies: Load in HEADER
+/* --------------------------
+<link rel="stylesheet" href="https://unpkg.com/buefy/lib/buefy.min.css">
+<link rel="stylesheet" href="//cdn.materialdesignicons.com/2.0.46/css/materialdesignicons.min.css">
 
+/// Dependencies: Load in FOOTER
+/* --------------------------
+<script src="https://unpkg.com/buefy"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.9/vue.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.3.4/vue-resource.min.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+----------------------------- */
 
 //////////////
 // FIREBASE //
 //////////////
-const firebaseConfig = {
-  apiKey: 'AIzaSyACYJ2gQFAaOIQHUQKGuChJy_uJjpUtW8o',
-  authDomain: 'routr-acd70.firebaseapp.com',
-  databaseURL: 'https://routr-acd70.firebaseio.com',
-  projectId: 'routr-acd70',
-  storageBucket: 'routr-acd70.appspot.com',
-  messagingSenderId: '705237275855'
+let config = {
+	apiKey: "AIzaSyDzsVMjDu-MI2j1-FbH3OPRZW6ei7m0BO8",
+	authDomain: "marshal-program.firebaseapp.com",
+	databaseURL: "https://marshal-program.firebaseio.com",
+	projectId: "marshal-program",
+	storageBucket: "marshal-program.appspot.com",
+	messagingSenderId: "426786350308"
 };
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
+
 const googleAuth = new firebase.auth.GoogleAuthProvider();
 const database = firebase.database();
 
-console.log("HEY! MARHSALCORE IS UP VIA RAWGIT.");
+
+
+/////////
+// VUE //
+/////////
+Vue.use( Buefy.default );
+let App = new Vue({
+    el: '#app',
+    data: {
+        switchState: true,
+        checkboxState: true
+    },
+    mounted: function(){
+    	this.$dialog.alert('Welcome vua BUEFY');
+    }
+});
+
